893dd4cf19a3927d54b57a63546e4363704f2729
Joonas Koivunen <joonas.koivunen@gmail.com>
2020-06-17 15:08:52 +0300

    fix: redirection in setup.sh, comments

--- setup.sh
+++ setup.sh
@@ -6,5 +6,11 @@ set -o pipefail
 npm install
 
 if [ -d "patches" ]; then
-    git apply patches/* -p3 --directory node_modules/interface-ipfs-core/
+    echo "Applying patches..."
+    # as we want to apply the patches create in a js-ipfs checkout to our node_modules
+    # we'll need to remove a few leading path segments to match
+    # a/packages/interface-ipfs-core/src/refs.js to node_modules/interface-ipfs-core/src/refs.js
+    #
+    # applying these patches on js-ipfs checkout does not require any extra arguments.
+    git apply --verbose patches/* -p3 --directory node_modules/interface-ipfs-core/
 fi
